aff96a2308264a09b5be1285fa5ec40fb9a6eb1b
Fah2542 <Chatboolta2542@gmail.com>
2021-11-23 20:24:52 +0700

    first commit

---
+++ dist/Chatbunta_Q1.js
@@ -0,0 +1,11 @@
+let employees;
+let emp = [
+    [1001, "Anurak", 5, 25000, 0, ""],
+    [1001, "Kongpop", 3, 20000, 0, ""],
+    [1001, "Pantira", 4, 17000, 0, ""],
+    [1001, "Tanthai", 0, 15000, 0, ""],
+    [1001, "Wasok", 5, 25000, 0, ""],
+];
+employees.forEach((i) => {
+    console.log(i);
+});
